1ecfe8f98ecfe8c553303c9a1e73b8724f244e06
Maria <mary.m0rhw@gmail.com>
2024-08-01 23:56:40 -0300

    conection sql

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const conection = require('./database/database');
+
+conection.authenticate().then(() => {
+    console.log('Conexão com o banco de dados realizada com sucesso');
+});
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -17,7 +26,10 @@ app.get('/question', (req, res) => {
 });
 
 app.post('/savequestion', (req, res) => {
-    res.send('formulario recebudo');
+    var title = req.body.title;
+    var description = req.body.description;
+
+    res.send('formulario recebido titulo: ' + title + ' descricao: ' + description);
 });
 
 app.listen(8080, () => {
